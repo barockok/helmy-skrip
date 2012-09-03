3725bb4384496376d82433ff0488dcca2ae258c7
Zidni Mubarock <zidmubarock@gmail.com>
2012-09-03 22:16:25 +0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fayad::Application.config.secret_token = '801e24f624fd45e530fba3b40e38bcaa5651d3b64a1d2e77b5c32f58d5c11c92ba82f341745dd0e404d63db97bfec7e00422713deaac86962fd40beab03f24f7'

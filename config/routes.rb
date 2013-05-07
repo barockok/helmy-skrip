a864e26f655f7682ed37e4e5e082b45b35f1bf33
Zidni Mubarock <zidmubarock@gmail.com>
2013-05-08 06:53:36 +0700

    devise an strong parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 HelmySkrip::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations", :passwords => "users/passwords"}
 
   resources :transactions
 

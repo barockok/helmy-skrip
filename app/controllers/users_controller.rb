bc5c6b449b887a153154bc885d9dc522441d808c
Zidni Mubarock <zidmubarock@gmail.com>
2013-05-15 22:49:58 +0700

    show error when add admin not valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,9 +49,9 @@ class	UsersController < InternalController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @nasabah }
+        format.json { render action: 'show', status: :created, location: @user }
       else
-        format.html { render action: 'new' }
+        format.html { flash[:error] =  @user.errors.to_a.join(', ');  render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end

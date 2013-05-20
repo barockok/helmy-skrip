eb814e4027ddb415a6b7c86ad9ff419fee3da660
Zidni Mubarock <zidmubarock@gmail.com>
2013-05-21 00:38:26 +0700

    this is Jakarta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include ApplicationHelper
+  before_filter do
+    Time.zone = 'Asia/Jakarta'
+  end
+
 end
